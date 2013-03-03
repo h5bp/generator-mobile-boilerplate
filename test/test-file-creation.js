906d1434f48c24e99691c1339c0e5de402c851ae
Sindre Sorhus <sindresorhus@gmail.com>
2013-03-03 21:15:18 +0100

    Fix tests

--- test/test-file-creation.js
+++ test/test-file-creation.js
@@ -4,15 +4,15 @@ var path = require('path');
 var helpers = require('yeoman-generator').test;
 
 describe('H5BP Mobile Boilerplate generator', function () {
-	beforeEach(function(cb) {
-		var deps = ['../../app', [helpers.createDummyGenerator(), 'mobile-boilerplate:app']];
+	beforeEach(function (cb) {
+		var deps = ['../../app'];
 
 		helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
 			if (err) {
 				return cb(err);
 			}
 
-			this.mb = helpers.createGenerator('mobile-boilerplate:app', deps);
+			this.generator = helpers.createGenerator('mobile-boilerplate:app', deps);
 			cb();
 		}.bind(this));
 	});
@@ -20,13 +20,10 @@ describe('H5BP Mobile Boilerplate generator', function () {
 	it('generates expected files', function (cb) {
 		var expected = ['index.html', 'README.md'];
 
-		helpers.mockPrompt(this.mb, { docs: 'y' });
+		helpers.mockPrompt(this.generator, { docs: 'y' });
 
-		this.mb.sourceRoot(path.join(__dirname, 'temp'));
-
-		this.mb.run({}, function () {
-			// TODO: figure out why this doesn't work
-			//helpers.assertFiles(expected);
+		this.generator.run({}, function () {
+			helpers.assertFiles(expected);
 			cb();
 		});
 	});
